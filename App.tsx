bd78998a8c8f485d592af77019896fec16a5397e
Benson <Benson.xyzz@gmail.com>
2021-08-05 18:22:09 +0800

    fix: 🐛 环境问题

--- App.tsx
+++ App.tsx
@@ -1,25 +1,14 @@
 import 'react-native-gesture-handler';
-// import './src/utils/errorHandler';
-
-// import React from 'react';
-// import Main from './src/index';
-// import { RootSiblingParent } from 'react-native-root-siblings';
-
-// export default function App() {
-//   return (
-//     <RootSiblingParent>
-//       <Main />
-//     </RootSiblingParent>
-//   );
-// }
+import './src/utils/errorHandler';
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import Main from './src/index';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
 export default function App() {
   return (
-    <View>
-      <Text>1111</Text>
-    </View>
+    <RootSiblingParent>
+      <Main />
+    </RootSiblingParent>
   );
 }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
-// import { Assets } from '@react-navigation/elements';
+import { Assets } from '@react-navigation/elements';
 import { registerRootComponent } from 'expo';
-// import { Asset } from 'expo-asset';
+import { Asset } from 'expo-asset';
 
 import App from './App';
 
-// Asset.loadAsync(Assets);
+Asset.loadAsync(Assets);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
