cfc733f5d8a34c90e4b93f770b237b57e7e8323e
Benson <Benson.xyzz@gmail.com>
2021-08-18 17:23:47 +0800

    feat: 🎸 代码优化

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -5,14 +5,17 @@ import { getUserInfo } from '../api';
 export default function useUserInfo({ userId }: { userId: string }) {
   const [userInfo, setUserInfo] = React.useState({});
   React.useEffect(() => {
+    let ignore = false;
     const fetchData = async () => {
       const { result, data, message } = await getUserInfo.fetch(userId);
-      if (result === 'success') {
+      if (result === 'success' && !ignore) {
         setUserInfo(data);
       }
     };
     fetchData();
-    return () => {};
+    return () => {
+      ignore = true;
+    };
   }, [userId]);
 
   return userInfo;
